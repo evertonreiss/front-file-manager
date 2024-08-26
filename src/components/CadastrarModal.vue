c2fda61326068b08a3e35ef49c4d8e12ef8007bf
Everton Reis <joaoeverton2015@gmail.com>
2024-08-26 01:35:35 -0300

    style: personalização dos modals cadastro e login

--- src/components/CadastrarModal.vue
+++ src/components/CadastrarModal.vue
@@ -3,65 +3,70 @@ import { api } from '@/utils/api';
 import { ref } from 'vue';
 import { useAuthStore } from '@/utils/authStore';
 
-const authStore = useAuthStore()
-const dialog = ref(false)
+const authStore = useAuthStore();
+const dialog = ref(false);
 
-const name = ref('Everton')
-const email = ref('everton@email.com')
-const password = ref('password')
+const name = ref('Everton');
+const email = ref('everton@email.com');
+const password = ref('password');
 
 async function cadastrar() {
     const data = {
         "name": name.value,
         "email": email.value,
         "password": password.value
-    }
+    };
     try {
-        const response = await api.post('/register', data)
+        const response = await api.post('/register', data);
         if (response.status == 201) {
-            authStore.login(email.value, password.value)
-            dialog = false
+            authStore.login(email.value, password.value);
+            dialog.value = false;
         }
     } catch (error) {
-
+        console.error(error);
     }
 }
-
 </script>
+
 <template>
-    <div class="pa-4 text-center">
+    <div class="px-3 text-center">
         <v-dialog v-model="dialog" max-width="500">
             <template v-slot:activator="{ props: activatorProps }">
-                <v-btn class="text-none font-weight-regular" text="Cadastrar" variant="tonal"
-                    v-bind="activatorProps"></v-btn>
+                <v-btn class="text-none font-weight-regular" text="Cadastrar" v-bind="activatorProps"></v-btn>
             </template>
 
-            <v-card prepend-icon="mdi-account" title="Cadastrar-se">
+            <v-card prepend-icon="mdi-account" color="#303540" title="Cadastrar-se">
                 <v-card-text>
-                    <v-row dense>
-                        <v-col cols="12" md="12" sm="6">
-                            <v-text-field v-model="name" label="Nome" required></v-text-field>
-                        </v-col>
+                    <!-- Adicione o formulário -->
+                    <form @submit.prevent="cadastrar">
+                        <v-row dense>
+                            <v-col cols="12" md="12" sm="6">
+                                <v-text-field v-model="name" label="Nome" required variant="outlined"></v-text-field>
+                            </v-col>
 
-                        <v-col cols="12" md="12" sm="6">
-                            <v-text-field v-model="email" label="Email" type="email" required></v-text-field>
-                        </v-col>
+                            <v-col cols="12" md="12" sm="6">
+                                <v-text-field v-model="email" label="Email" type="email" required
+                                    variant="outlined"></v-text-field>
+                            </v-col>
 
-                        <v-col cols="12" md="12" sm="6">
-                            <v-text-field v-model="password" label="Senha" type="password" required></v-text-field>
-                        </v-col>
-                    </v-row>
+                            <v-col cols="12" md="12" sm="6">
+                                <v-text-field v-model="password" label="Senha" type="password" required
+                                    variant="outlined"></v-text-field>
+                            </v-col>
+                        </v-row>
+
+                        <v-divider></v-divider>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+
+                            <v-btn @click="dialog = false" class=" text-none text-white" color="#50505070" rounded="2"
+                                variant="flat" text="Cancelar" type="submit"></v-btn>
+                            <v-btn class="text-none ms-2 text-white" color="#1e4f3e" rounded="2" variant="flat"
+                                text="Cadastrar" type="submit"></v-btn>
+                        </v-card-actions>
+                    </form>
                 </v-card-text>
-
-                <v-divider></v-divider>
-
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-
-                    <v-btn text="Cancelar" variant="plain" @click="dialog = false"></v-btn>
-
-                    <v-btn color="primary" text="Cadastrar" variant="tonal" @click="cadastrar"></v-btn>
-                </v-card-actions>
             </v-card>
         </v-dialog>
     </div>
--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -1,51 +1,56 @@
 <script setup>
 import { ref } from 'vue';
 import { useAuthStore } from '@/utils/authStore';
-const dialog = ref(false)
-const email = ref('xxevertonreisxx@gmail.com')
-const password = ref('password')
-const authStore = useAuthStore()
+
+const dialog = ref(false);
+const email = ref('everton@email.com');
+const password = ref('password');
+const authStore = useAuthStore();
 
 async function login() {
     try {
-        authStore.login(email.value, password.value)
-
+        await authStore.login(email.value, password.value);
+        dialog.value = false;
     } catch (error) {
-
+        console.error(error);
     }
 }
-
 </script>
+
 <template>
-    <div class="pa-1 text-center">
+    <div class="text-center">
         <v-dialog v-model="dialog" max-width="500">
             <template v-slot:activator="{ props: activatorProps }">
-                <v-btn class="text-none font-weight-regular" text="Login" variant="tonal"
-                    v-bind="activatorProps"></v-btn>
+                <v-btn class="text-none font-weight-regular" text="Login" v-bind="activatorProps"></v-btn>
             </template>
 
-            <v-card prepend-icon="mdi-account" title="Fazer login">
+            <v-card prepend-icon="mdi-account" title="Fazer login" color="#303540">
                 <v-card-text>
-                    <v-row dense>
-                        <v-col cols="12" md="12" sm="6">
-                            <v-text-field v-model="email" label="Email" required></v-text-field>
-                        </v-col>
+                    <!-- Adicione o formulário -->
+                    <form @submit.prevent="login">
+                        <v-row dense>
+                            <v-col cols="12" md="12" sm="6">
+                                <v-text-field v-model="email" label="Email" required variant="outlined"></v-text-field>
+                            </v-col>
 
-                        <v-col cols="12" md="12" sm="6">
-                            <v-text-field v-model="password" label="Senha" type="password" required></v-text-field>
-                        </v-col>
-                    </v-row>
+                            <v-col cols="12" md="12" sm="6">
+                                <v-text-field v-model="password" label="Senha" type="password" required
+                                    variant="outlined"></v-text-field>
+                            </v-col>
+                        </v-row>
+
+                        <v-divider></v-divider>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+
+                            <v-btn @click="dialog = false" class=" text-none text-white" color="#50505070" rounded="2"
+                                variant="flat" text="Cancelar" type="submit"></v-btn>
+                            <v-btn class="text-none ms-2 text-white" color="#1e4f3e" rounded="2" variant="flat"
+                                text="Login" type="submit"></v-btn>
+                        </v-card-actions>
+                    </form>
                 </v-card-text>
-
-                <v-divider></v-divider>
-
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-
-                    <v-btn text="Cancelar" variant="plain" @click="dialog = false"></v-btn>
-
-                    <v-btn color="primary" text="Login" variant="tonal" @click="login"></v-btn>
-                </v-card-actions>
             </v-card>
         </v-dialog>
     </div>
