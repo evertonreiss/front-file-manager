2ef6d21c7a76d5b633ec2e78275e35c846549d5e
Everton Reis <joaoeverton2015@gmail.com>
2024-08-25 22:57:17 -0300

    feat: adição do vuetify na aplicação e remoção de arquivos não utilizados

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,78 @@
 <script setup>
-import Header from './components/Header.vue';
+import { ref, watch } from 'vue'
+import LoginModal from './components/LoginModal.vue';
+import RegisterModal from './components/RegisterModal.vue';
+import { useAuthStore } from './utils/authStore';
+import ListagemArquivo from './components/ListagemArquivo.vue';
+
+const drawer = ref(false)
+const group = ref(null)
+const authStore = useAuthStore()
+
+const items = ref([
+    {
+        title: 'Todos',
+        value: 'all'
+    },
+    {
+        title: 'Imagens',
+        value: 'image',
+    },
+    {
+        title: 'Vídeos',
+        value: 'video',
+    },
+    {
+        title: 'Documentos',
+        value: 'document',
+    },
+    {
+        title: 'Músicas',
+        value: 'music',
+    },
+])
+
+watch(group, () => {
+    drawer.value = false
+})
 </script>
 
 <template>
-    <Header></Header>
+    <v-card>
+        <v-layout>
+            <v-app-bar color="#253545" prominent>
+                <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+                <v-toolbar-title>File Manager</v-toolbar-title>
+
+                <v-spacer></v-spacer>
+
+                <template v-if="!authStore.isAuthenticated()">
+                    <LoginModal></LoginModal>
+                    <RegisterModal></RegisterModal>
+                </template>
+                <template v-else>
+                    <p style="margin: 0 15px;">
+                        Bem vindo, {{ authStore.getUser()?.name }}
+                    </p>
+                    <v-btn text="Sair" variant="plain" @click="authStore.logout()"></v-btn>
+                </template>
+
+            </v-app-bar>
+
+            <v-navigation-drawer color="#213141" v-model="drawer"
+                :location="$vuetify.display.mobile ? 'left' : undefined" temporary>
+                <v-list :items="items"></v-list>
+            </v-navigation-drawer>
+
+            <v-main style="height: 100vh; background-color: #333; color: aliceblue;">
+                <v-card-text>
+                    <ListagemArquivo></ListagemArquivo>
+                </v-card-text>
+            </v-main>
+        </v-layout>
+    </v-card>
 </template>
+
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+    },
+})
+
 const app = createApp(App)
+
+app.use(vuetify)
 
 app.use(router)
 
